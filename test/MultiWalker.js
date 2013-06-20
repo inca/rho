71a67ac0c6827f671865bb1e47f8bf7b8a0f20c1
inca <incarnate@whiteants.net>
2013-06-20 17:52:50 +0400

    Added MultiWalker and coevered its ass with tests.

---
+++ test/MultiWalker.js
@@ -0,0 +1,72 @@
+"use strict";
+var assert = require('assert')
+  , Walker = require('../lib/walker').Walker
+  , SubWalker = require('../lib/walker').SubWalker
+  , MultiWalker = require('../lib/walker').MultiWalker;
+
+var pangram = "The quick brown fox jumps over the lazy dog.";
+
+describe('MultiWalker', function() {
+
+  var w1 = new SubWalker(pangram, 0, 10);  // "The quick "
+  var w2 = new SubWalker(pangram, 16, 35);   //  "fox jumps over the "
+  var w3 = new SubWalker(pangram, 10, 16);   //  "brown "
+  var w4 = "cat.";
+
+  describe('when composed from another walkers', function() {
+
+    var w = new MultiWalker([w1, w2, w3, w4]);
+
+    it('should evaluate proper #length', function() {
+      assert.equal(w.length, 39);
+    });
+
+    it('should evaluate proper #toString', function() {
+      assert.equal(w.toString(), "The quick fox jumps over the brown cat.");
+    });
+
+    it('should evaluate proper #charAt', function() {
+      assert.equal(w.charAt(2), "e");
+      assert.equal(w.charAt(9), " ");
+      assert.equal(w.charAt(10), "f");
+      assert.equal(w.charAt(31), "o");
+    });
+
+    it('should evaluate proper #substring', function() {
+      assert.equal(w.substring(4, 13), "quick fox");
+      assert.equal(w.substring(29, 38), "brown cat");
+      assert.equal(w.substring(10, 10), "");
+      assert.equal(w.substring(0, w.length), w.toString());
+    });
+
+    it('should evaluate proper #absoluteIndex', function() {
+      assert.equal(w.absoluteIndex(29), 10);
+      assert.equal(w.absoluteIndex(10), 16);
+    });
+
+  });
+
+  describe('when composed from nested multi-walkers', function() {
+
+    var m1 = new MultiWalker([w2, w3]);
+    var m2 = new MultiWalker([w1, m1, w4]);
+
+    it('should evaluate correctly', function() {
+      assert.equal(m2.toString(), "The quick fox jumps over the brown cat.");
+    });
+
+    it('should not break inside another walker', function() {
+      var w = new SubWalker(m2, 10, 34);
+      assert.equal(w.toString(), "fox jumps over the brown");
+      assert.equal(w.length, 24);
+      assert.equal(w.charAt(10), "o");
+      assert.equal(w.substring(15, 24), "the brown");
+      assert.equal(w.substring(0, 0), "");
+      assert.equal(w.substring(0, w.length), w.toString());
+    });
+
+  });
+
+
+
+});
