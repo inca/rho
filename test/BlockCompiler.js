e66dace8a267b0ca006d6b5b0c44132b01de0434
inca <incarnate@whiteants.net>
2013-07-02 14:11:41 +0400

    Added test for UL with selectors.

--- test/BlockCompiler.js
+++ test/BlockCompiler.js
@@ -10,23 +10,26 @@ describe('BlockCompiler', function() {
     // Block tests are prettified to save on whitespace misunderstandings
     var c = new BlockCompiler({ pretty: true });
 
-    var samplesDir = __dirname + "/samples";
+    var samplesDir = __dirname + "/../samples";
     var samples = fs.readdirSync(samplesDir);
 
     for (var i in samples) {
       var filename = samples[i];
-      if (/\.rho$/.test(filename)) {
-        var sample = filename.replace(/\.rho$/, "");
-        it('process "' + sample + '"', function() {
-          var text = fs.readFileSync(
-            samplesDir + "/" + filename, { encoding: 'utf-8' });
-          var expectedHtml = fs.readFileSync(
-            samplesDir + "/" + sample + ".html",
-            { encoding: 'utf-8'}).trim();
-          var actualHtml = c.toHtml(text).trim();
-          assert.equal(expectedHtml, actualHtml);
-        });
-      }
+      if (/\.rho$/.test(filename))
+        testFile(filename);
+    }
+
+    function testFile(filename) {
+      var sample = filename.replace(/\.rho$/, "");
+      it('process "' + sample + '"', function() {
+        var text = fs.readFileSync(
+          samplesDir + "/" + filename, { encoding: 'utf-8' });
+        var expectedHtml = fs.readFileSync(
+          samplesDir + "/" + sample + ".html",
+          { encoding: 'utf-8'}).trim();
+        var actualHtml = c.toHtml(text).trim();
+        assert.equal(expectedHtml, actualHtml);
+      });
     }
 
   });
